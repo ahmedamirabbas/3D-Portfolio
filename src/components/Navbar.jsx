2c5b4771b37d82d5163802c156dcae2640ad5c76
Ahmed Amir <ahmad.abdulkhalig@gmail.com>
2024-06-26 14:37:09 +0200

    added play and blog links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,17 @@ const Navbar = () => {
         <p className="blue-gradient_text">AA</p>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
-        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
+        <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" } target="blank">
           About
         </NavLink>
-        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"}>
+        <NavLink to='/projects' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"} target="blank">
           Projects
+        </NavLink>
+        <NavLink to='https://ahmedamir7.itch.io' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"} target="blank">
+          Play
+        </NavLink>
+        <NavLink to='https://medium.com/@ahmad.abdulkhalig' className={({ isActive }) => isActive ? "text-blue-600" : "text-black"} target="blank">
+          Blog
         </NavLink>
       </nav>
     </header>
